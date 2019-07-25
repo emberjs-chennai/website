84239531e84a7fa64c60c1b6a5379337ece4d660
Robin Philip Thomas <robinphilipthomas@outlook.in>
2019-07-25 19:12:36 +0530

    updating schedule and content changes

--- app/data/schedule.js
+++ app/data/schedule.js
@@ -3,76 +3,46 @@ export default [
     startTime: "09:00 am",
     endTime: "09:45 am",
     name: "Registration",
-    topicDesc: "After the registration at the Freshworks front desk. Come to Freshworks ground floor Cafeteria."
+    topicDesc: "After the registration. Come to Freshworks ground floor Cafeteria."
   },
   {
-    startTime: "10:00 am",
-    endTime: "10:30 am",
+    startTime: "",
+    endTime: "",
     name: "Octane: Why and How",
     speaker: "Gokul Kathirvel",
     speakerPic: "assets/images/gokul.png",
-    topicDesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries."
+    topicDesc: "A Talk about how Ember Octane, a new edition of Ember.js will help frontend developers to spin modern web apps really easily and quickly. You'll know how the Octane edition utilizes the native platform features like ES Classes, decorators, etc., effectively to write modern apps and how the entire framework aligns itself with the native ecosystem."
   },
   {
-    startTime: "10:30 am",
-    endTime: "11:00 am",
-    name: "coming soon",
-    speaker: "",
-    speakerPic: "assets/images/coming-soon.png",
-    topicDesc: ""
+    startTime: "",
+    endTime: "",
+    name: "Differential bundling of assets in Ember",
+    speaker: "Sivasubramanyam A",
+    speakerPic: "assets/images/Sivasubramanyam_A.jpeg",
+    topicDesc: "This talk proposes a technique to serve different bundles of app assets transpiled according to the user's browser. This results in a reduced bundle size and faster parsing."
   },
   {
-    startTime: "11:15 am",
-    endTime: "11:45 am",
-    name: "Ember-Accessibility",
-    speaker: "Kandha Vivek Raj",
-    speakerPic: "assets/images/kandha-vivek-raj.png",
-    topicDesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries."
-  },
-  {
-    startTime: "12:00 am",
-    endTime: "12:30 am",
+    startTime: "",
+    endTime: "",
     name: "Web performance in 2019",
     speaker: "Gopal Venkatesan",
     speakerPic: "assets/images/gopal.png",
-    topicDesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries."
+    topicDesc: "Even in 2019 JavaScript is the most expensive resource when compared against HTML, CSS or other media.  In this talk the speaker will walk through various techniques to reduce JavaScript payload to increase the performance of your website.  Also we will learn various tools available to measure the performance and find out bottlenecks of your website."
   },
   {
-    startTime: "12:30 am",
-    endTime: "01:30 pm",
-    name: "Lunch",
-    isBreak: true
+    startTime: "",
+    endTime: "",
+    name: "Ember Accessibility",
+    speaker: "Kandha Vivek Raj K",
+    speakerPic: "assets/images/kandha-vivek-raj.png",
+    topicDesc: "Accessibility is an important aspect of applications. This add-on makes it easy to develop accessible apps by providing contextual feedback wherever necessary."
   },
   {
-    startTime: "01:30 pm",
-    endTime: "01:45 am",
-    name: "lightning talk 1",
-    speaker: "",
-    speakerPic: "",
-    topicDesc: ""
-  },
-  {
-    startTime: "01:45 pm",
-    endTime: "02:00 pm",
-    name: "lightning talk 2",
-    speaker: "",
-    speakerPic: "",
-    topicDesc: ""
-  },
-  {
-    startTime: "02:00 pm",
-    endTime: "02:30 pm",
-    name: "Coming soon..",
-    speaker: "",
-    speakerPic: "assets/images/coming-soon.png",
-    topicDesc: ""
-  },
-  {
-    startTime: "02:00 pm",
-    endTime: "03:00 pm",
-    name: "Closing keynote",
-    speaker: "",
-    speakerPic: "assets/images/coming-soon.png",
+    startTime: "",
+    endTime: "",
+    name: "Planting Abstract Syntax Trees",
+    speaker: "Rajasegar Chandiran",
+    speakerPic: "assets/images/Rajasegar.jpg",
     topicDesc: ""
   }
 ]
