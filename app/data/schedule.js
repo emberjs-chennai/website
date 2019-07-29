8f3ebb9218c126b2b4eae9a060e90001449c8742
Robin Philip Thomas <robinphilipthomas@outlook.in>
2019-07-29 19:45:17 +0530

    update schedule and image

--- app/data/schedule.js
+++ app/data/schedule.js
@@ -7,7 +7,7 @@ export default [
   },
   {
     startTime: "10:00 am",
-    endTime: "10:30am",
+    endTime: "10:30 am",
     name: "Octane: Why and How",
     speaker: "Gokul Kathirvel",
     speakerPic: "assets/images/gokul.jpg",
@@ -16,10 +16,10 @@ export default [
   {
     startTime: "10:30 am",
     endTime: "11:00 am",
-    name: "Differential bundling of assets in Ember",
-    speaker: "Sivasubramanyam A",
-    speakerPic: "assets/images/siva.jpg",
-    topicDesc: "This talk proposes a technique to serve different bundles of app assets transpiled according to the user's browser. This results in a reduced bundle size and faster parsing."
+    name: "Web performance in 2019",
+    speaker: "Gopal Venkatesan",
+    speakerPic: "assets/images/gopal.jpeg",
+    topicDesc: "Even in 2019 JavaScript is the most expensive resource when compared against HTML, CSS or other media.  In this talk the speaker will walk through various techniques to reduce JavaScript payload to increase the performance of your website.  Also we will learn various tools available to measure the performance and find out bottlenecks of your website."
   },
   {
     startTime: "11:00 am",
@@ -29,40 +29,40 @@ export default [
   },
   {
     startTime: "11:10 am",
-    endTime: "11:40 am",
-    name: "Web performance in 2019",
-    speaker: "Gopal Venkatesan",
-    speakerPic: "assets/images/gopal.jpeg",
-    topicDesc: "Even in 2019 JavaScript is the most expensive resource when compared against HTML, CSS or other media.  In this talk the speaker will walk through various techniques to reduce JavaScript payload to increase the performance of your website.  Also we will learn various tools available to measure the performance and find out bottlenecks of your website."
-  },
-  {
-    startTime: "11:40 am",
-    endTime: "11:50 am",
+    endTime: "11:20 am",
     name: "Ember Accessibility",
     speaker: "Kandha Vivek Raj K",
     speakerPic: "assets/images/vivek.jpg",
     topicDesc: "Accessibility is an important aspect of applications. This add-on makes it easy to develop accessible apps by providing contextual feedback wherever necessary."
   },
   {
-    startTime: "11:00 am",
-    endTime: "11:10 am",
+    startTime: "11:20 am",
+    endTime: "11:50 am",
+    name: "Differential bundling of assets in Ember",
+    speaker: "Sivasubramanyam A",
+    speakerPic: "assets/images/siva.jpg",
+    topicDesc: "This talk proposes a technique to serve different bundles of app assets transpiled according to the user's browser. This results in a reduced bundle size and faster parsing."
+  },
+  {
+    startTime: "11:50 am",
+    endTime: "12:00 pm",
     name: "Break",
     isBreak: true
   },
   {
     startTime: "12:00 pm",
     endTime: "12:30 pm",
-    name: "Planting Abstract Syntax Trees",
-    speaker: "Rajasegar Chandiran",
-    speakerPic: "assets/images/Rajasegar.jpg",
-    topicDesc: ""
+    name: "JAM Stack",
+    speaker: "Kanjarla Praneeth",
+    speakerPic: "assets/images/kanjarla-praneeth.jpeg",
+    topicDesc: "Evaluation of JAM stack and how rapidly it's occupying the market.how is the market opportunities  and what ,where to learn"
   },
   {
     startTime: "12:30 pm",
     endTime: "01:00 pm",
-    name: "JAM Stack",
-    speaker: "Kanjarla praneeth",
-    speakerPic: "assets/images/kanjarla-praneeth.jpeg",
-    topicDesc: "Evaluation of JAM stack and how rapidly it's occupying the market.how is the market opportunities  and what ,where to learn"
+    name: "ASTs & Emberjs",
+    speaker: "Rajasegar Chandiran",
+    speakerPic: "assets/images/Rajasegar.jpg",
+    topicDesc: "A talk about Abstract Syntax Trees, and how Ember.js community makes use of AST tooling."
   }
 ]
