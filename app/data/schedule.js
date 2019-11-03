ad3447a5fafe2ea61408d29e2e22762368a32551
Robin Philip Thomas <robint@Robin-mac.local>
2019-11-03 16:37:28 +0530

    update website content

--- app/data/schedule.js
+++ app/data/schedule.js
@@ -7,41 +7,33 @@ export default [
   },
   {
     startTime: "10:00 am",
-    endTime: "10:30 am",
-    name: "Octane: Why and How?",
-    speaker: "Gokul Kathirvel",
-    speakerPic: "assets/images/gokul.jpg",
-    topicDesc: "A Talk about how Ember Octane, a new edition of Ember.js will help frontend developers to spin modern web apps really easily and quickly. You'll know how the Octane edition utilizes the native platform features like ES Classes, decorators, etc., effectively to write modern apps and how the entire framework aligns itself with the native ecosystem."
+    endTime: "10:40 am",
+    name: "Write Tests Like a Mathematician",
+    speaker: "Isaac Lee",
+    speakerPic: "",
+    topicDesc: ""
   },
   {
-    startTime: "10:30 am",
-    endTime: "11:00 am",
-    name: "Web performance in 2019",
-    speaker: "Gopal Venkatesan",
-    speakerPic: "assets/images/gopal.jpeg",
-    topicDesc: "Even in 2019 JavaScript is the most expensive resource when compared against HTML, CSS or other media.  In this talk the speaker will walk through various techniques to reduce JavaScript payload to increase the performance of your website.  Also we will learn various tools available to measure the performance and find out bottlenecks of your website."
+    startTime: "10:40 am",
+    endTime: "11:10 am",
+    name: "Building Real Time Applications with Ember",
+    speaker: "Sivasubramanyam A",
+    speakerPic: "",
+    topicDesc: ""
   },
   {
     startTime: "11:00 am",
-    endTime: "11:10 am",
+    endTime: "11:20 am",
     name: "Break",
     isBreak: true
   },
   {
-    startTime: "11:10 am",
-    endTime: "11:20 am",
-    name: "Ember Accessibility",
-    speaker: "Kandha Vivek Raj K",
-    speakerPic: "assets/images/vivek.jpg",
-    topicDesc: "Accessibility is an important aspect of applications. This add-on makes it easy to develop accessible apps by providing contextual feedback wherever necessary."
-  },
-  {
     startTime: "11:20 am",
     endTime: "11:50 am",
-    name: "Differential bundling of assets in Ember",
-    speaker: "Sivasubramanyam A",
-    speakerPic: "assets/images/siva.jpg",
-    topicDesc: "This talk proposes a technique to serve different bundles of app assets transpiled according to the user's browser. This results in a reduced bundle size and faster parsing."
+    name: "Building Testable & Performant Ember Apps",
+    speaker: "Dilip Kushwaha, Manu Pandit",
+    speakerPic: "",
+    topicDesc: ""
   },
   {
     startTime: "11:50 am",
@@ -50,19 +42,27 @@ export default [
     isBreak: true
   },
   {
-    startTime: "12:00 pm",
+    startTime: "12:00 am",
+    endTime: "12:15 am",
+    name: "A safe journey from Component to Add-on!",
+    speaker: "Varun Ananth",
+    speakerPic: "",
+    topicDesc: ""
+  },
+  {
+    startTime: "12:15 pm",
     endTime: "12:30 pm",
-    name: "JAM Stack",
-    speaker: "Kanjarla Praneeth",
-    speakerPic: "assets/images/kanjarla-praneeth.jpeg",
-    topicDesc: "Evaluation of JAM stack and how rapidly it's occupying the market.how is the market opportunities  and what ,where to learn"
+    name: "'Clean Code' in Ember world",
+    speaker: "VasanthaKumar",
+    speakerPic: "",
+    topicDesc: ""
   },
   {
     startTime: "12:30 pm",
     endTime: "01:00 pm",
-    name: "ASTs & Emberjs",
-    speaker: "Rajasegar Chandiran",
-    speakerPic: "assets/images/Rajasegar.jpg",
-    topicDesc: "A talk about Abstract Syntax Trees, and how Ember.js community makes use of AST tooling."
+    name: "Detect and fix leaks",
+    speaker: "Kandha Vivek Raj",
+    speakerPic: "",
+    topicDesc: ""
   }
 ]
