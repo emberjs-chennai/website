12e6104776c2ea610ee4e7e5b711708cf54c5bcb
Robin Philip Thomas <robint@Robin-mac.local>
2019-11-05 14:56:38 +0530

    update schedule and remove sponsor page

--- app/data/schedule.js
+++ app/data/schedule.js
@@ -7,15 +7,15 @@ export default [
   },
   {
     startTime: "10:00 am",
-    endTime: "10:40 am",
+    endTime: "10:30 am",
     name: "Write Tests Like a Mathematician",
     speaker: "Isaac Lee",
     speakerPic: "",
     topicDesc: ""
   },
   {
-    startTime: "10:40 am",
-    endTime: "11:10 am",
+    startTime: "10:30 am",
+    endTime: "11:00 am",
     name: "Building Real Time Applications with Ember",
     speaker: "Sivasubramanyam A",
     speakerPic: "",
@@ -23,37 +23,37 @@ export default [
   },
   {
     startTime: "11:00 am",
-    endTime: "11:20 am",
+    endTime: "11:10 am",
     name: "Break",
     isBreak: true
   },
   {
-    startTime: "11:20 am",
-    endTime: "11:50 am",
-    name: "Building Testable & Performant Ember Apps",
-    speaker: "Dilip Kushwaha, Manu Pandit",
+    startTime: "11:00 am",
+    endTime: "11:25 pm",
+    name: "'Clean Code' in Ember world",
+    speaker: "VasanthaKumar",
     speakerPic: "",
     topicDesc: ""
   },
   {
-    startTime: "11:50 am",
-    endTime: "12:00 pm",
-    name: "Break",
-    isBreak: true
-  },
-  {
-    startTime: "12:00 am",
-    endTime: "12:15 am",
+    startTime: "11:25 am",
+    endTime: "11:45 am",
     name: "A safe journey from Component to Add-on!",
     speaker: "Varun Ananth",
     speakerPic: "",
     topicDesc: ""
   },
   {
-    startTime: "12:15 pm",
+    startTime: "11:45 am",
+    endTime: "12:00 pm",
+    name: "Break",
+    isBreak: true
+  },
+  {
+    startTime: "12:00 pm",
     endTime: "12:30 pm",
-    name: "'Clean Code' in Ember world",
-    speaker: "VasanthaKumar",
+    name: "Building Testable & Performant Ember Apps",
+    speaker: "Dilip Kushwaha, Manu Pandit",
     speakerPic: "",
     topicDesc: ""
   },
